93f8ac2448dd3b5f4d50f78534035f958dfadbf0
lalatina11 <rahmadancandra@gmail.com>
2025-06-10 07:37:50 +0700

    feat: apply create pin controller

--- src/routes/pin.ts
+++ src/routes/pin.ts
@@ -2,8 +2,8 @@ import express from "express"
 import pinController from "../controller/pin"
 const route = express()
 
-route.post("/create", pinController.create)
 route.get("/", pinController.getAallPins)
+route.post("/", pinController.create)
 route.get("/:id", pinController.getPin)
 
 export default route
